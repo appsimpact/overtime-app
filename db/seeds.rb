4f028469e40886d4f8ea249c73a3da51355aee63
Shashi Kashyap <shashi.aimit@hotmail.com>
2019-06-25 13:11:23 +0530

    display list of posts via bdd

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,12 @@
+@user = User.create!(
+  email: "test@test.com",
+  password: "password",
+  password_confirmation: "password",
+  first_name: "Jon",
+  last_name: "Snow"
+)
+
 50.times do |index|
-  Post.create!(date: Date.today, rationale: "#{index} rationale content")
+  Post.create!(date: Date.today, rationale: "#{index} rationale content", user_id: @user.id)
 end
 puts '50 posts have been created'
